559a8c71b228ceb281e8ab8c7de47ca3556f6262
AhmedYasser136 <117648009+AhmedYasser136@users.noreply.github.com>
2022-11-08 14:22:00 +0200

    Add files via upload

---
+++ LAB 3/lab3-1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num=0;
+    int sum = 0;
+    while (sum <=100)
+    {
+
+        printf("int num ");
+        scanf("%d", &num);
+        sum+=num;
+
+    }
+//    printf("sum",sum);
+    return 0;
+}
