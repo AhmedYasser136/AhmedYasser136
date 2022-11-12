3a94b5f6d8ae2b1ce3dce1f06cad93e261f2846e
Ahmed Yasser <eng.ahmed.yasser136@gmail.com>
2022-11-12 23:24:37 +0200

    lab7

---
+++ proj 7-1/texteditor.h
@@ -0,0 +1,8 @@
+#ifndef text_
+#define text_
+void textattr(int ForgC);
+void gotoxy(int x,int y);
+void move(char event,int size);
+void delet(char * text);
+void edit (char *text,char event);
+#endif // text_
